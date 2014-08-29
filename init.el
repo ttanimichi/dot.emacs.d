677da01559667974afbaaf6a6f4c873f2bfe7b5c
Tsukuru Tanimichi <tanimichi@outlook.jp>
2014-08-29 17:16:27 +0900

    whitespace-mode

--- init.el
+++ init.el
@@ -362,3 +362,12 @@
 (add-hook 'haml-mode-hook 'rainbow-mode)
 (add-hook 'js-mode-hook 'rainbow-mode)
 (add-hook 'coffee-mode-hook 'rainbow-mode)
+
+;; タブや全角スペースを目立たせる
+(require 'whitespace)
+(setq whitespace-style '(face tabs tab-mark spaces space-mark))
+(setq whitespace-display-mappings '((space-mark ?\u3000 [?\u25a1]) (tab-mark ?\t [?\xBB ?\t] [?\\ ?\t])))
+(setq whitespace-space-regexp "\\(\u3000+\\)")
+(set-face-foreground 'whitespace-tab "DimGrey")
+(set-face-foreground 'whitespace-space "DimGrey")
+(global-whitespace-mode 1)
